5dea7ebbefcbcd23d3ce59f73fcde1bd845a6b88
felixGlez <felixgonzalezgarcia2010@gmail.com>
2024-05-07 22:18:09 +0200

    WIP

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -1,12 +1,61 @@
 import { useLocation } from 'react-router-dom';
-import { StyledBackButton, StyledCountryDetails } from './styles';
+import {
+	StyledBackButton,
+	StyledBorderCountry,
+	StyledCountryData,
+	StyledCountryDetails,
+	StyledCountryName,
+	StyledFlag,
+	StyledInfo,
+	StyledInfoContainer
+} from './styles';
 
 const CountryDetails = () => {
-	const { state } = useLocation();
-	console.log(state);
+	const { state: country } = useLocation();
 	return (
 		<StyledCountryDetails>
-			<StyledBackButton to={'/'}>Back</StyledBackButton>
+			<StyledBackButton to={'/'}>
+				<img src='/shape.png' />
+				Back
+			</StyledBackButton>
+			<StyledInfoContainer>
+				<StyledFlag src={country.flags.svg} />
+				<StyledInfo>
+					<StyledCountryName>{country.name.common}</StyledCountryName>
+					<StyledCountryData>
+						<b>Native Name:</b> {country.name.official}
+					</StyledCountryData>
+					<StyledCountryData>
+						<b>Population:</b> {country.population}
+					</StyledCountryData>
+					<StyledCountryData>
+						<b>Region:</b> {country.region}
+					</StyledCountryData>
+					<StyledCountryData>
+						<b>Sub Region:</b> {country.subregion}
+					</StyledCountryData>
+					<StyledCountryData>
+						<b>Capital:</b> {country.capital}
+					</StyledCountryData>
+					<StyledCountryData>
+						<b>Top Level Domain:</b> {country.tld.join(', ')}
+					</StyledCountryData>
+					<StyledCountryData>
+						<b>Currencies:</b>{' '}
+						{Object.values(country.currencies).map(
+							currency => `${currency.name}(${currency.symbol}) `
+						)}
+					</StyledCountryData>
+					<StyledCountryData>
+						<b>Languages:</b> {Object.values(country.languages).join(', ')}
+					</StyledCountryData>
+					<b>Border Countries:</b>
+					{country?.borders?.map(border => (
+						<StyledBorderCountry key={border}>{border}</StyledBorderCountry>
+					))}
+					{!country.borders && <span>No Borders Countries</span>}
+				</StyledInfo>
+			</StyledInfoContainer>
 		</StyledCountryDetails>
 	);
 };
--- src/pages/styles.js
+++ src/pages/styles.js
@@ -7,11 +7,74 @@ const StyledCountryDetails = styled.div`
 	justify-content: center;
 	align-items: center;
 	padding: 2rem 5rem;
+
+	@media (max-width: 769px) {
+		padding: 0rem;
+	}
 `;
 
 const StyledBackButton = styled(NavLink)`
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	width: 8.5rem;
 	box-shadow: 0px 0px 7px 2px rgba(0, 0, 0, 0.0294);
 `;
 
-export { StyledCountryDetails, StyledBackButton };
+const StyledInfoContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+
+	@media (max-width: 769px) {
+		width: 100%;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+`;
+
+const StyledFlag = styled.img`
+	width: 45%;
+	display: flex;
+	justify-content: center;
+	@media (max-width: 769px) {
+		width: 90%;
+	}
+`;
+
+const StyledInfo = styled.div`
+	width: 45%;
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+
+	@media (max-width: 769px) {
+		width: 90%;
+	}
+`;
+
+const StyledCountryName = styled.h1`
+	font-size: 2rem;
+`;
+
+const StyledCountryData = styled.span`
+	font-size: 1rem;
+`;
+
+const StyledBorderCountry = styled.div`
+	display: flex;
+	justify-content: center;
+	width: 80px;
+	box-shadow: 0px 0px 7px 2px rgba(0, 0, 0, 0.0294);
+`;
+
+export {
+	StyledCountryDetails,
+	StyledBackButton,
+	StyledInfoContainer,
+	StyledFlag,
+	StyledInfo,
+	StyledCountryName,
+	StyledCountryData,
+	StyledBorderCountry
+};
